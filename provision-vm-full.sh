99bf76e606e928a4aa9e0e5d6aed66ebe9da5694
Tony Fitzsimmons <tony@fitzsimmons.com.au>
2026-04-30 13:36:20 +1000

    Update provisioning script with cloud-init Ansible install

--- provision-vm-full.sh
+++ provision-vm-full.sh
@@ -59,7 +59,7 @@ ANSIBLE_TARGET_NODE="prox-two"
 ANSIBLE_IP="10.110.10.8"
 
 # NFT Test Server
-NFT_VM_ID="102"
+NFT_VM_ID="101"
 NFT_NAME="nft-test"
 NFT_CPU="4"
 NFT_RAM="8192"
@@ -68,13 +68,13 @@ NFT_TARGET_NODE="prox-one"
 NFT_IP="10.110.10.7"
 
 # FlippiQ Production
-FLIPPIق_VM_ID="101"
-FLIPPIق_NAME="flippiق-prod"
-FLIPPIق_CPU="8"
-FLIPPIق_RAM="16384"
-FLIPPIق_DISK="400"
-FLIPPIق_TARGET_NODE="prox-one"
-FLIPPIق_IP="10.110.10.6"
+FLIPPIQ_VM_ID="100"
+FLIPPIQ_NAME="flippiq-prod"
+FLIPPIQ_CPU="8"
+FLIPPIQ_RAM="16384"
+FLIPPIQ_DISK="400"
+FLIPPIQ_TARGET_NODE="prox-one"
+FLIPPIQ_IP="10.110.10.6"
 
 ################################################################################
 # FUNCTIONS
@@ -105,7 +105,7 @@ function download_debian_iso() {
   
   log_info "Downloading Debian 12 ISO to $target_node..."
   
-  ssh -i "$PROX_SSH_KEY" "${PROX_SSH_USER}@${target_ip}" bash << 'SSH_COMMANDS'
+  ssh -i "$PROX_SSH_KEY" "${PROX_SSH_USER}@${target_ip}" bash << 'DOWNLOAD_ISO'
 DEBIAN_ISO_FILENAME="debian-12.13.0-amd64-netinst.iso"
 ISO_PATH="/var/lib/vz/template/iso/${DEBIAN_ISO_FILENAME}"
 
@@ -117,7 +117,7 @@ else
   wget -q "https://cdimage.debian.org/cdimage/archive/12.13.0/amd64/iso-cd/debian-12.13.0-amd64-netinst.iso"
   echo "ISO downloaded successfully"
 fi
-SSH_COMMANDS
+DOWNLOAD_ISO
 
   log_info "Debian ISO ready on $target_node"
 }
@@ -133,3 +133,99 @@ function create_vm() {
   log_info "Creating VM $vm_id ($vm_name) on $target_node..."
   
   ssh -i "$PROX_SSH_KEY" "${PROX_SSH_USER}@${target_ip}" qm create $vm_id \
+    --name $vm_name \
+    --cores $cpu_cores \
+    --memory $ram_mb \
+    --machine q35 \
+    --ostype l26 \
+    --scsihw virtio-scsi-pci \
+    --net0 virtio,bridge=vmbr0 \
+    --boot order=ide2 \
+    --ide2 local:iso/debian-12.13.0-amd64-netinst.iso,media=cdrom \
+    --agent enabled=1 \
+    --onboot 1
+  
+  log_info "VM $vm_id created successfully on $target_node"
+}
+
+function cleanup_iso() {
+  local target_node="$1"
+  local target_ip="${PROX_IPS[${target_node}]}"
+  
+  log_info "Cleaning up ISO from $target_node..."
+  
+  ssh -i "$PROX_SSH_KEY" "${PROX_SSH_USER}@${target_ip}" \
+    rm -f /var/lib/vz/template/iso/debian-12.13.0-amd64-netinst.iso
+  
+  log_info "ISO cleanup complete"
+}
+
+################################################################################
+# MAIN MENU
+################################################################################
+
+function show_menu() {
+  echo ""
+  echo "===================================================="
+  echo "Proxmox VM Provisioning - FlippiQ Infrastructure"
+  echo "Author: Tony Fitzsimmons (tonyfitzs)"
+  echo "===================================================="
+  echo ""
+  echo "Select server type to provision:"
+  echo ""
+  echo "  1) Ansible Control Node (prox-two, 201, 4 CPU, 8GB RAM)"
+  echo "  2) NFT Test Server (prox-one, 101, 4 CPU, 8GB RAM)"
+  echo "  3) FlippiQ Production (prox-one, 100, 8 CPU, 16GB RAM)"
+  echo "  4) Exit"
+  echo ""
+  read -p "Enter choice [1-4]: " choice
+}
+
+################################################################################
+# MAIN EXECUTION
+################################################################################
+
+function main() {
+  while true; do
+    show_menu
+    
+    case $choice in
+      1)
+        log_info "Building Ansible Control Node..."
+        download_debian_iso "$ANSIBLE_TARGET_NODE"
+        create_vm "$ANSIBLE_VM_ID" "$ANSIBLE_NAME" "$ANSIBLE_CPU" "$ANSIBLE_RAM" "$ANSIBLE_TARGET_NODE"
+        log_info "VM $ANSIBLE_VM_ID created. Static IP: $ANSIBLE_IP"
+        log_info "Boot the VM and complete Debian installation"
+        read -p "Press Enter after OS installation is complete: "
+        cleanup_iso "$ANSIBLE_TARGET_NODE"
+        ;;
+      2)
+        log_info "Building NFT Test Server..."
+        download_debian_iso "$NFT_TARGET_NODE"
+        create_vm "$NFT_VM_ID" "$NFT_NAME" "$NFT_CPU" "$NFT_RAM" "$NFT_TARGET_NODE"
+        log_info "VM $NFT_VM_ID created. Static IP: $NFT_IP"
+        log_info "Boot the VM and complete Debian installation"
+        read -p "Press Enter after OS installation is complete: "
+        cleanup_iso "$NFT_TARGET_NODE"
+        ;;
+      3)
+        log_info "Building FlippiQ Production Server..."
+        download_debian_iso "$FLIPPIQ_TARGET_NODE"
+        create_vm "$FLIPPIQ_VM_ID" "$FLIPPIQ_NAME" "$FLIPPIQ_CPU" "$FLIPPIQ_RAM" "$FLIPPIQ_TARGET_NODE"
+        log_info "VM $FLIPPIQ_VM_ID created. Static IP: $FLIPPIQ_IP"
+        log_info "Boot the VM and complete Debian installation"
+        read -p "Press Enter after OS installation is complete: "
+        cleanup_iso "$FLIPPIQ_TARGET_NODE"
+        ;;
+      4)
+        log_info "Exiting..."
+        exit 0
+        ;;
+      *)
+        echo "Invalid choice. Please try again."
+        ;;
+    esac
+  done
+}
+
+main "$@"
