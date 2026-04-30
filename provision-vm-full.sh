70dc1364a4e11cb781b31b9f94fc3bed9f2f86e0
Tony Fitzsimmons <34207637+tonyfitzs@users.noreply.github.com>
2026-04-30 13:56:47 +1000

    Update version to 1.5 and add VM configurations
    Updated version number and added configuration for Debian 12 ISO and VM specifications.

--- provision-vm-full.sh
+++ provision-vm-full.sh
@@ -4,7 +4,7 @@
 # Proxmox VM Provisioning Script for FlippiQ Infrastructure
 # Author: Tony Fitzsimmons (tonyfitzs)
 # Purpose: Provision Ansible, NFT test, and FlippiQ production VMs
-# Version: 1.4
+# Version: 1.5
 # License: MIT
 ################################################################################
 
@@ -39,17 +39,44 @@ trap cleanup EXIT
 # CONFIGURATION
 ################################################################################
 
-# Proxmox cluster nodes
 declare -A PROX_IPS=(
   [prox-one]="10.110.10.3"
   [prox-two]="10.110.10.4"
   [prox-three]="10.110.10.5"
 )
 
-# Network configuration
 GATEWAY="10.0.110.1"
 DNS_PRIMARY="10.0.110.1"
 DNS_SECONDARY="8.8.4.4"
 SUBNET_MASK="24"
 
-# Debian 12 ISO
+DEBIAN_ISO_URL="https://cdimage.debian.org/cdimage/archive/12.13.0/amd64/iso-cd/debian-12.13.0-amd64-netinst.iso"
+DEBIAN_ISO_FILENAME="debian-12.13.0-amd64-netinst.iso"
+
+PROX_SSH_USER="root"
+PROX_SSH_KEY="${PROX_SSH_KEY:-/path/to/proxmox/private/key}"
+
+STORAGE_LOCAL="local-lvm"
+STORAGE_SHARED="zpool"
+
+ANSIBLE_VM_ID="201"
+ANSIBLE_NAME="ansible-ctrl"
+ANSIBLE_CPU="4"
+ANSIBLE_RAM="8192"
+ANSIBLE_DISK="50"
+ANSIBLE_TARGET_NODE="prox-two"
+ANSIBLE_IP="10.110.10.8"
+
+NFT_VM_ID="101"
+NFT_NAME="nft-test"
+NFT_CPU="4"
+NFT_RAM="8192"
+NFT_DISK="100"
+NFT_TARGET_NODE="prox-one"
+NFT_IP="10.110.10.7"
+
+FLIPPIQ_VM_ID="100"
+FLIPPIQ_NAME="flippiq-prod"
+FLIPPIQ_CPU="8"
+FLIPPIQ_RAM="16384"
+FLIPPIQ_DISK="400"
